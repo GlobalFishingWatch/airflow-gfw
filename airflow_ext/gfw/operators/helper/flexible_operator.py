ae93505b313ed72499b2335715ae74684397c026
Matias Piano <matias@globalfishingwatch.org>
2019-07-25 12:52:15 -0300

    Validates parameters and accept to pass extra_parameters

--- airflow_ext/gfw/operators/helper/flexible_operator.py
+++ airflow_ext/gfw/operators/helper/flexible_operator.py
@@ -6,26 +6,35 @@ import os
 
 class FlexibleOperator:
     def __init__(self, parameters):
+        assert parameters
         self.operator_parameters = parameters
 
     def build_operator(self, kind):
         if kind != 'bash' or kind != 'kubernetes':
             raise ValueError('The operators allowed are bash or kubernetes, not <{}>'.format(kind))
-        operator = None
+        assert self.operator_parameters['task_id']
+        assert self.operator_parameters['pool']
+        assert self.operator_parameters['cmds']
+        assert self.operator_parameters['image']
         task_id = self.operator_parameters['task_id']
         pool = self.operator_parameters['pool']
         cmds = self.operator_parameters['cmds']
+        operator = None
         if kind == 'bash':
+            assert self.operator_parameters['docker_run']
             commands = '{} {} {}'.format(
                 self.operator_parameters['docker_run'],
                 self.operator_parameters['image'],
                 ' '.join(cmds))
+            # Left only the extra parameters that are not the bash_command
+            extra_params = { k : self.operator_parameters[k] for k in set(self.operator_parameters) - set(dict.fromkeys({'cmds','image','docker_run'})) }
             operator = BashOperator(
-                task_id = task_id,
-                pool = pool,
-                bash_command = commands
+                bash_command = commands,
+                **extra_params
             )
         else:
+            assert self.operator_parameters['name']
+            assert self.operator_parameters['dag']
             commands = ['./scripts/run.sh']
             commands.append(cmds)
             operator = KubernetesPodOperator(
