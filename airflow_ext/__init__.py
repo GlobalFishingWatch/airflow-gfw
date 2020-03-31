241b5416790fa3ea49f95245a5743b38184a1f21
Matias Piano <matias@globalfishingwatch.org>
2020-03-31 05:30:17 -0300

    Updates the version and the CHANGES.md

--- airflow_ext/__init__.py
+++ airflow_ext/__init__.py
@@ -3,7 +3,7 @@ Airflow extension for GFW pipelines.
 """
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Matias Piano'
 __email__ = 'matias@globalfishingwatch.org'
 __source__ = 'https://github.com/GlobalFishingWatch/airflow-gfw'
