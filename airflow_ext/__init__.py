8043796caf11372a6d07b1464b056224d2ef9a20
Matias Piano <matias@globalfishingwatch.org>
2021-04-28 03:35:18 -0300

    Adds the fix to parse correctly the urlto get the Dataflow job id and let knowing the status of the DF job from Airflow side for function wait_for_done()

--- airflow_ext/__init__.py
+++ airflow_ext/__init__.py
@@ -3,7 +3,7 @@ Airflow extension for GFW pipelines.
 """
 
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __author__ = 'Matias Piano'
 __email__ = 'matias@globalfishingwatch.org'
 __source__ = 'https://github.com/GlobalFishingWatch/airflow-gfw'
